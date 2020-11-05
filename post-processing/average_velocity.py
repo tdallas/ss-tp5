dc52301e50430d324918776a86eae354c6e5766e
tdorado <tdorado@itba.edu.ar>
2020-11-05 17:01:13 -0300

    deleted old post prossesing and added average velocity

---
+++ post-processing/average_velocity.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from parser_xyz import XYZParser
+
+
+file = 'out/output300.xyz'
+parsed_data = XYZParser(file)
+average_velocities = []
+time = []
+
+for iteration in parsed_data.get_output():
+    velocities = []
+    time.append(iteration[0].get_time_passed())
+    for particle in iteration:
+        velocities.append(particle.get_velocity())
+    average_velocities.append(np.average(velocities))
+
+plt.plot(time, average_velocities)
+plt.xlabel('Tiempo [s]', fontsize=16)
+plt.ylabel('Velocidad [m/s]', fontsize=16)
+plt.title('Velocidad promedio')
+plt.ylim(0, 1.55)
+plt.tight_layout()
+plt.show()
--- post-processing/particle.py
+++ post-processing/particle.py
@@ -2,18 +2,17 @@ from math import sqrt
 
 
 class Particle:
-    def __init__(self, id, x_position, y_position, x_velocity, y_velocity, radius, mass, time_passed):
+    def __init__(self, id, x_position, y_position, x_velocity, y_velocity, radius, time_passed):
         self.id = int(id)
         self.x_position = float(x_position)
         self.y_position = float(y_position)
         self.x_velocity = float(x_velocity)
         self.y_velocity = float(y_velocity)
         self.radius = float(radius)
-        self.mass = float(mass)
         self.time_passed = float(time_passed)
 
     def __str__(self):
-        return self.x_position + ' | ' + self.y_position
+        return str(self.x_position) + ' | ' + str(self.y_position)
 
     def get_x_position(self):
         return self.x_position
@@ -32,9 +31,6 @@ class Particle:
 
     def get_id(self):
         return self.id
-
-    def get_mass(self):
-        return self.mass
 
     def get_radius(self):
         return self.radius
